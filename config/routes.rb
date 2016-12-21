e8a4bd178b2b7979dcdab4eed46405c26d40d244
Yuichi Masuda <1995.phoenix@gmail.com>
2016-12-21 22:34:26 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :tournaments
-
+  resources :tournaments do
+    resources :participant_teams do
+      resources :teammates
+    end
+  end
+  
   root :to => 'tournaments#index'
 end
