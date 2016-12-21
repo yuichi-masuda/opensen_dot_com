1413461e099e4671c284163708b76f0d9ca800ae
Yuichi Masuda <1995.phoenix@gmail.com>
2016-12-21 23:21:01 +0900

    viewを作成

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,14 +1,24 @@
 class TournamentsController < ApplicationController
   def index
+    @tournaments = Tournament.all
   end
 
   def show
+    @tournament = Tournament.find(params[:id])
   end
 
   def new
+    @tournament = Tournament.new
   end
 
   def create
+    @tournament = Tournament.new(tournament_params)
+
+    if @tournament.save
+      redirect_to @tournament, notice: '大会を作成しました'
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,5 +28,10 @@ class TournamentsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def tournament_params
+    params.require(:tournament).permit(:name, :event_date, :place, :athletic_event, :note, :deadline, :published)
   end
 end
