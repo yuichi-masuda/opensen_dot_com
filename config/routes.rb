316a4c86ae694bf0bd6e086b1dc808d7e18c39cb
Yuichi Masuda <1995.phoenix@gmail.com>
2016-12-21 23:29:47 +0900

    コントローラー諸々作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,32 @@
 Rails.application.routes.draw do
+  get 'teammates/index'
+
+  get 'teammates/new'
+
+  get 'teammates/create'
+
+  get 'teammates/edit'
+
+  get 'teammates/show'
+
+  get 'teammates/update'
+
+  get 'teammates/destroy'
+
+  get 'participant_teams/index'
+
+  get 'participant_teams/new'
+
+  get 'participant_teams/create'
+
+  get 'participant_teams/edit'
+
+  get 'participant_teams/show'
+
+  get 'participant_teams/update'
+
+  get 'participant_teams/destroy'
+
   resources :tournaments do
     resources :participant_teams do
       resources :teammates
